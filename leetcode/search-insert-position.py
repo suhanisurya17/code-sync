3491f2cb3fb4e44388b9f139196575219d81fc18
Suhani Surya <111797378+suhanisurya17@users.noreply.github.com>
2025-10-22 14:09:14 -0400

    Optimize searchInsert with binary search
    Refactor searchInsert method to use binary search for efficiency.

--- leetcode/search-insert-position.py
+++ leetcode/search-insert-position.py
@@ -1,3 +1,4 @@
+#more inefficient solution O(n) time complexity
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         #super simple question with case handling
@@ -11,3 +12,21 @@ class Solution:
                 if target > num:
                     counter += 1
             return counter
+
+#more efficient solution harnessing the power of binary sort
+
+class Solution:
+    import bisect
+    def searchInsert(self, nums:List[int], target:int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else: #this part does the binary sort algorithm
+            position = bisect.bisect_left(nums, target)
+            if position == 0:
+                return 0
+            elif position == len(nums)
+                return len(nums)
+        else:
+            before = nums[position - 1]
+            after = nums[position]
+            return before if abs(before - position) < abs(after - position) else after
