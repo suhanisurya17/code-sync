38898f8de83bd2ee3752d88810be7a427ecc0f83
Suhani Surya <111797378+suhanisurya17@users.noreply.github.com>
2025-11-24 01:28:25 -0500

    Refactor groupAnagrams to use sorted strings
    Refactor groupAnagrams method to use sorted strings for grouping.

--- leetcode/group-anagrams.py
+++ leetcode/group-anagrams.py
@@ -1,3 +1,4 @@
+#using tuple
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)  # mapping char count to list of anagrams
@@ -7,3 +8,15 @@ class Solution:
                 count[ord(char) - ord("a")] += 1
             anagrams[tuple(count)].append(string)
         return list(anagrams.values())
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #creating a dictionary (key value pair still) + MUST SPECIFY DATA TYPE OF VALUE
+        anagrams = defaultdict(list)
+
+        #iterating through each string
+        for string in strs:
+            #sorted list sorts the characters in ascending order so ex: "eat" -> {['a', 'e', 't']: value}
+            sorted_list = ''.join(sort(string))
+            anagrams[sorted_list].append(string) #"eat" -> {['a', 'e', 't']: ["eat"]}
+        return list(anagrams.values()) #returning thr list version of the anagrams values, since each value is a list of the grouped anagrams
